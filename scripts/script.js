a95ebc20c3135aa259d61b504a93d9e12c376583
BronwenM <bronwen.marchant@gmail.com>
2021-09-23 18:41:36 -0400

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", initializeApp);
+
+function initializeApp() {
+    displayMeme();
+    displayTodayDate();
+};
+
+async function displayMeme() {
+    try {
+        const response = await fetch("https://api.imgflip.com/get_memes");
+        const jsonResponse = await response.json();
+
+        //required
+        console.log(jsonResponse);
+        const meme = jsonResponse.data.memes[1];
+
+        const image = document.createElement("img");
+        image.src = meme.url;
+        image.alt = meme.name;
+        image.width = 200;
+
+        const memeContainer = document.querySelector("#meme");
+        memeContainer.appendChild(image);
+    }
+    catch(error) {
+        console.log("Promise failed", error);
+    }
+};
+
+function displayTodayDate() {
+    const today = new Date();
+    document.getElementById("today").innerHTML = "(Today is " + today.toDateString() + ")";
+}
